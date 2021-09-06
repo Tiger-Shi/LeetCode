5458c89f946aaf2c67baec53978fd81be28060a3
Tiger-Shi <1745694739@qq.com>
2021-09-06 16:11:48 +0800

    we

---
+++ test.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class test {
+    public static void main(String[] args) {
+
+        Date date=new Date(2021-1900,12-1,31);
+        System.out.println(date.getMonth());
+
+    }
+}
