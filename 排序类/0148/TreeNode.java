85ab45596e682d59674f354d1f4246c48b736269
Tiger-Shi <1745694739@qq.com>
2021-12-29 11:56:08 +0800

    12-29

--- 排序类/0148/TreeNode.java
+++ 排序类/0148/TreeNode.java
@@ -1,28 +1,49 @@
+import java.util.*;
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
 
-// Definition for a Node.
-class Node {
-    public int val;
-    public Node left;
-    public Node right;
-    public Node next;
-    public Node() {}
-    public Node(int _val) {
-        val = _val;
+    TreeNode() {
     }
 
-    public Node(int _val, Node _left, Node _right, Node _next) {
-        val = _val;
-        left = _left;
-        right = _right;
-        next = _next;
+    TreeNode(int val) {
+        this.val = val;
     }
-};
 
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
 
 class Solution {
-    public Node connect(Node root) {
-        Node result=null;
+    public LinkedList<TreeNode> generatetrees(int start,int end){
+        LinkedList<TreeNode> all_trees=new LinkedList<TreeNode>();
+        if(start>end){
+            all_trees.add(null);
+            return all_trees;
+        }
+        for(int i=start;i<=end;i++){
+            LinkedList<TreeNode> left_tree=generatetrees(start,i-1);
+            LinkedList<TreeNode> rigth_tree=generatetrees(i+1, end);
+            for(TreeNode left:left_tree ){
+                for(TreeNode right:rigth_tree){
+                    TreeNode current_tree=new TreeNode(i);
+                    current_tree.left=left;
+                    current_tree.right=right;
+                    all_trees.add(current_tree);
+                }
+            }
+        }
+        return all_trees;
+    }
 
-        return result;
+    public List<TreeNode> generateTrees(int n) {
+        if(n==0){
+            return new LinkedList<TreeNode>();
+        }
+        return generatetrees(1,n);
     }
 }
